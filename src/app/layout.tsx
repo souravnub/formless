c1880d8472bce24f7d8c2a2eb1c596ad9c7ddb96
souravnub <s6hr6h4u3@gmail.com>
2024-09-06 20:15:52 -0600

    feat: initialize app & add shadcnUI

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Formless",
+    description:
+        "Transform your form management. Streamline physical forms into a sleek digital format with ease—no more clutter, just efficient and intuitive handling.",
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={`${inter.className} antialiased`}>{children}</body>
+        </html>
+    );
+}
