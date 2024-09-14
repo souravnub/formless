dd0fef5606e9daf351d9867dfd865978e7d20788
souravnub <s6hr6h4u3@gmail.com>
2024-09-13 23:07:17 -0600

    feat: add frontend routes

---
+++ src/app/user/forms/[id]/submissions/new/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const FormSubmissionPage = () => {
+    // page where the user will be submitting a submission for the form
+    return <div>FormSubmissionPage</div>;
+};
+
+export default FormSubmissionPage;
