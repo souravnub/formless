2b243ef318630677b727353717bc8f1731012fe7
souravnub <s6hr6h4u3@gmail.com>
2024-09-30 21:46:46 -0600

    feat: pdf for multiple submissions can be generated

--- src/app/admin/submissions/page.tsx
+++ src/app/admin/submissions/page.tsx
@@ -4,7 +4,7 @@ import CustomBreadcrumb from "@/components/CustomBreadcrumb";
 import { DatePickerWithRange } from "@/components/DatePicker";
 import { DateFilterSelect } from "@/components/filterSelect";
 import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
     Pagination,
     PaginationContent,
@@ -32,6 +32,9 @@ type Submissions = Prisma.FormSubmissionGetPayload<{
 
 export default function SubmissionsPage() {
     const [submissions, setSubmissions] = useState<Submissions[]>([]);
+    const [selectedSubmissions, setSelectedSubmissions] = useState<number[]>(
+        []
+    );
     const [loading, setLoading] = useState(true);
 
     const fetchSubmissions = async () => {
@@ -39,6 +42,7 @@ export default function SubmissionsPage() {
         setSubmissions(submissions);
         setLoading(false);
     };
+
     useEffect(() => {
         fetchSubmissions();
     }, []);
@@ -56,99 +60,113 @@ export default function SubmissionsPage() {
                 <h1 className="text-xl font-medium items-center">
                     Manage Submissions
                 </h1>
-                <div className="flex flex-col items-center">
-                    <h1 className="mb-4">Filtering</h1>
-                    <div className="flex space-x-4">
-                        <DateFilterSelect />
-                        <DatePickerWithRange />
-                    </div>
+                <div className="flex space-x-4">
+                    {selectedSubmissions.length > 0 && (
+                        <Button>
+                            <Link
+                                href={`/admin/submissions/pdf?submissions=${selectedSubmissions.join(
+                                    ","
+                                )}`}>
+                                Generate Report
+                            </Link>
+                        </Button>
+                    )}
+                    <DateFilterSelect />
+                    <DatePickerWithRange />
                 </div>
             </div>
-            <Card className="mb-2">
-                <CardHeader>Submissions</CardHeader>
-                <CardContent>
-                    <Table className="border">
-                        <TableHeader className="bg-accent/70">
-                            <TableRow>
-                                <TableHead className="pl-5">
-                                    User's Name
-                                </TableHead>
-                                <TableHead>Contact Info</TableHead>
-                                <TableHead>Form Name</TableHead>
-                                <TableHead>Date Submitted</TableHead>
-                                <TableHead>Last Edited</TableHead>
-                                <TableHead>Actions</TableHead>
-                            </TableRow>
-                        </TableHeader>
-                        <TableBody>
-                            {submissions.map((submission) => (
-                                <TableRow
-                                    key={submission.id}
-                                    className="hover:bg-none!important">
-                                    <TableCell className="font-medium pl-5">
-                                        {submission.user.name}
-                                    </TableCell>
-                                    <TableCell>
-                                        {submission.user.email}
-                                    </TableCell>
-                                    <TableCell>
-                                        {submission.form.title}
-                                    </TableCell>
-                                    <TableCell>
-                                        {new Date(
-                                            submission.updatedAt
-                                        ).toLocaleDateString()}
-                                    </TableCell>
-                                    <TableCell>
-                                        {new Date(
-                                            submission.createdAt
-                                        ).toLocaleDateString()}
-                                    </TableCell>
-                                    <TableCell className="flex gap-2">
-                                        <Button asChild>
-                                            <Link
-                                                href={`/admin/submissions/${submission.id}`}>
-                                                View
-                                            </Link>
-                                        </Button>
-                                        <Button asChild variant={"default"}>
-                                            <Link
-                                                href={`/admin/submissions/${submission.id}/pdf`}>
-                                                Pdf
-                                            </Link>
-                                        </Button>
-                                        <Button asChild variant={"destructive"}>
-                                            <Link
-                                                href={
-                                                    "/admin/submissions/delete"
-                                                }>
-                                                Delete
-                                            </Link>
-                                        </Button>
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                    {/* meant for searching all submissions as required */}
-                    <Pagination>
-                        <PaginationContent>
-                            <PaginationItem>
-                                <PaginationPrevious href="#" />
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationLink href="#">1</PaginationLink>
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationEllipsis />
-                            </PaginationItem>
-                            <PaginationItem>
-                                <PaginationNext href="#" />
-                            </PaginationItem>
-                        </PaginationContent>
-                    </Pagination>
-                </CardContent>
-            </Card>
+            <Table className="border">
+                <TableHeader className="bg-accent/70">
+                    <TableRow>
+                        <TableHead className="w-10"></TableHead>
+                        <TableHead className="pl-5">User's Name</TableHead>
+                        <TableHead>Contact Info</TableHead>
+                        <TableHead>Form Name</TableHead>
+                        <TableHead>Date Submitted</TableHead>
+                        <TableHead>Last Edited</TableHead>
+                        <TableHead>Actions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {submissions.map((submission) => (
+                        <TableRow
+                            key={submission.id}
+                            className="hover:bg-none!important">
+                            <TableCell>
+                                <Checkbox
+                                    onCheckedChange={(checked) => {
+                                        if (checked) {
+                                            setSelectedSubmissions((prev) => [
+                                                ...prev,
+                                                submission.id,
+                                            ]);
+                                        } else {
+                                            setSelectedSubmissions((prev) =>
+                                                prev.filter(
+                                                    (prevSub) =>
+                                                        prevSub !==
+                                                        submission.id
+                                                )
+                                            );
+                                        }
+                                    }}
+                                />
+                            </TableCell>
+                            <TableCell className="font-medium pl-5">
+                                {submission.user.name}
+                            </TableCell>
+                            <TableCell>{submission.user.email}</TableCell>
+                            <TableCell>{submission.form.title}</TableCell>
+                            <TableCell>
+                                {new Date(
+                                    submission.updatedAt
+                                ).toLocaleDateString()}
+                            </TableCell>
+                            <TableCell>
+                                {new Date(
+                                    submission.createdAt
+                                ).toLocaleDateString()}
+                            </TableCell>
+                            <TableCell className="flex gap-2">
+                                <Button asChild>
+                                    <Link
+                                        href={`/admin/submissions/${submission.id}`}>
+                                        View
+                                    </Link>
+                                </Button>
+                                <Button asChild variant={"default"}>
+                                    <Link
+                                        href={`/admin/submissions/${submission.id}/pdf`}>
+                                        Pdf
+                                    </Link>
+                                </Button>
+                                <Button asChild variant={"destructive"}>
+                                    <Link href={"/admin/submissions/delete"}>
+                                        Delete
+                                    </Link>
+                                </Button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+            {/* meant for searching all submissions as required */}
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
         </div>
     );
 }
