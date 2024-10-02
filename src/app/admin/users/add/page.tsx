1969754a15f2f0bed8e0978cf3f5f86353530a67
AmrinderSinghKhangura <amrinder.khangura@edu.sait.ca>
2024-10-02 17:39:37 -0600

    frontend input validation for addUser

--- src/app/admin/users/add/page.tsx
+++ src/app/admin/users/add/page.tsx
@@ -8,87 +8,89 @@ import { useToast } from "@/hooks/use-toast";
 import React, { FormEvent, useState } from "react";
 
 const AddUserPage = () => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [role, setRole] = useState("USER");
-    const [password, setPassword] = useState("");
-    const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("USER");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-    const { toast } = useToast();
+  const { toast } = useToast();
 
-    async function handleAddUser(e: FormEvent) {
-        e.preventDefault();
-        setIsLoading(true);
-        const res = await createUser({
-            name,
-            email,
-            role: role as "USER" | "SUPERVISOR",
-            password,
-        });
-        setIsLoading(false);
+  async function handleAddUser(e: FormEvent) {
+    e.preventDefault();
+    setIsLoading(true);
+    const res = await createUser({
+      name,
+      email,
+      role: role as "USER" | "SUPERVISOR",
+      password,
+    });
+    setIsLoading(false);
 
-        toast({
-            variant: !res.success ? "destructive" : "default",
-            description: res.message,
-        });
-    }
+    toast({
+      variant: !res.success ? "destructive" : "default",
+      description: res.message,
+    });
+  }
 
-    return (
-        <form
-            onSubmit={handleAddUser}
-            className="container max-w-sm space-y-2 pt-10">
-            <div>
-                <Label htmlFor="name">Name</Label>
-                <Input
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                    id="name"
-                    name="name"
-                />
-            </div>
+  return (
+    <form
+      onSubmit={handleAddUser}
+      className="container max-w-sm space-y-2 pt-10"
+    >
+      <div>
+        <Label htmlFor="name">Name</Label>
+        <Input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          id="name"
+          name="name"
+          required
+        />
+      </div>
 
-            <div>
-                <Label htmlFor="email">Email</Label>
-                <Input
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    id="email"
-                    name="email"
-                />
-            </div>
+      <div>
+        <Label htmlFor="email">Email</Label>
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          id="email"
+          name="email"
+          type="email"
+          required
+        />
+      </div>
 
-            <div>
-                <Label htmlFor="pass">Password</Label>
-                <Input
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    id="pass"
-                    name="password"
-                />
-            </div>
+      <div>
+        <Label htmlFor="pass">Password</Label>
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          id="pass"
+          name="password"
+          required
+        />
+      </div>
 
-            <div>
-                <Label>Role</Label>
-                <RadioGroup
-                    name="role"
-                    defaultValue={role}
-                    onValueChange={setRole}>
-                    <div className="flex items-center space-x-2">
-                        <RadioGroupItem value="SUPERVISOR" id="SUPERVISOR" />
-                        <Label htmlFor="SUPERVISOR">Supervisor</Label>
-                    </div>
-                    <div className="flex items-center space-x-2">
-                        <RadioGroupItem value="USER" id="USER" />
-                        <Label htmlFor="USER">User</Label>
-                    </div>
-                </RadioGroup>
-            </div>
+      <div>
+        <Label>Role</Label>
+        <RadioGroup name="role" defaultValue={role} onValueChange={setRole}>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="SUPERVISOR" id="SUPERVISOR" />
+            <Label htmlFor="SUPERVISOR">Supervisor</Label>
+          </div>
+          <div className="flex items-center space-x-2">
+            <RadioGroupItem value="USER" id="USER" />
+            <Label htmlFor="USER">User</Label>
+          </div>
+        </RadioGroup>
+      </div>
 
-            <Button disabled={isLoading} className="!mt-6">
-                {!isLoading ? "Create user" : "Creating User..."}
-            </Button>
-        </form>
-    );
+      <Button disabled={isLoading} className="!mt-6">
+        {!isLoading ? "Create user" : "Creating User..."}
+      </Button>
+    </form>
+  );
 };
 
 export default AddUserPage;
