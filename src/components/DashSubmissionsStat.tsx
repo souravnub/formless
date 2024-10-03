f42d2b4512bf44f4f9e62e19c81961881e6a1c52
Chris-D259 <christopher.duke@edu.sait.ca>
2024-10-02 22:25:48 -0600

    Create DashSubmissions Stat Start

---
+++ src/components/DashSubmissionsStat.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+const DashSubmissionsStat = () => {
+    const [totalUsers, setTotalUsers] =  useState(0);
+    const [totalForms, setTotalForms] =  useState(0);
+
+    useEffect(() => {
+        
+    })
+}
