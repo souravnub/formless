df217c5fa8c1482e32a8507d8eb16b5e984616d0
Chris-D259 <christopher.duke@edu.sait.ca>
2024-10-02 22:47:29 -0600

    Added getSubmissions Count Method to Submissions

--- src/components/DashSubmissionsStat.tsx
+++ src/components/DashSubmissionsStat.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { useState, useEffect } from "react";
-
+import { getUserCountByRole } from "@/actions/users";
+import { get } from "http";
 const DashSubmissionsStat = () => {
     const [totalUsers, setTotalUsers] =  useState(0);
     const [totalForms, setTotalForms] =  useState(0);
 
     useEffect(() => {
-        
-    })
+        getUserCountByRole("USER").then((res) => {
+            if (res.success) {
+                setTotalUsers(res.data??0);
+            }
+        });
+    },[]);
+
 }
