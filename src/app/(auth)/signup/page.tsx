eb7002b6c2a87f826e58b64fc47e1266d218dc0d
rikuteq <elias.irons@edu.sait.ca>
2024-10-10 16:50:14 -0600

    signup page creation & foundation

---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,27 @@
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import SignupForm from "@/components/domains/auth/SignupForm";
+
+
+export default async function SignUpPage() {
+    return (
+        <div className="container max-w-2xl pt-20">
+            <Card>
+                <CardHeader className="space-y-2">
+                    <CardTitle>Create a User Request to Formless</CardTitle>
+                    <CardDescription>
+                        This is the official Form management console for KBM.
+                        <Link
+                            href="https://kbmcommercial.com/"
+                            className="underline hover:text-primary">
+                            https://kbmcommercial.com/
+                        </Link>
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <SignupForm/>
+                </CardContent>
+            </Card>
+        </div>
+    );
+}
