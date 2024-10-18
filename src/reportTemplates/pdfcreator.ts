d9e8a50b510064518d8eb16d8a5f0c81ba2224bd
Chris-D259 <christopher.duke@edu.sait.ca>
2024-10-18 12:47:04 -0600

    Slim Down PDF Creator

--- src/reportTemplates/pdfcreator.ts
+++ src/reportTemplates/pdfcreator.ts
@@ -1,37 +1,6 @@
-import docCreator from './doccreator';
-import { NextApiRequest,NextApiResponse } from 'next';
-import path from 'path';
-import fs from 'fs';
-import { NextResponse } from 'next/server';
 const topdf = require("docx2pdf-converter");
-export default async function handler(req: Request, res: Response) {
-    if (req.method === 'POST'){
-        const receivedData  = await req.json();
-        try{
-            //Generate the DOCX
-            const data = receivedData.dataJson.submission[0].submissions;
-            const inputFile = "flha_template.docx"; // This will have to be made variable later on
-            const docBuffer = await docCreator(inputFile, data);
-            const docPath = path.join(process.cwd(), 'public', 'output','flha.docx');
-            fs.writeFileSync(docPath, docBuffer);
-            
-            //Convert the DOCX to PDF
-            const pdfPath = path.join(process.cwd(), 'public', 'output','flha.pdf');
-            await topdf(docPath, pdfPath);
 
-            //return the PDF File
-            const pdfBuffer = fs.readFileSync(pdfPath);
-            const response = new Response(pdfBuffer, {
-                status:200,
-                headers: {
-                    'Content-Type': 'application/pdf',
-                    'Content-Disposition': 'attachment; filename=flha.pdf',
-                },
-        });
-        return response;
-    }catch(error){
-        console.error('Error generating document:', error);
-        return NextResponse.json({ error: 'Error generating document' });
-    }
+async function pdfCreator(inputPath:String,outputName:String){
+    topdf.convert(inputPath,outputName);
 }
-}
+export default pdfCreator;
