c055f743fe6253326333e3ce4bc1773ab0c30890
Chris-D259 <christopher.duke@edu.sait.ca>
2024-10-18 11:16:20 -0600

    Added PDF Gen Command

---
+++ src/actions/handlePDFDownload.ts
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+    const handlePDFDownload = async ({submissionId}:{submissionId:number}) =>{
+        try{
+            const data = await fetch(`/api/reportGen/submission?submissions=${submissionId}`,{
+                method: 'GET',
+                
+
+
+            });
+            if(!data.ok){
+                throw new Error('Error fetching data');
+            }
+            console.log(data);
+            const dataJson = await data.json();
+        
+        const generateDoc = await fetch('/api/reportGen/pdfGen',{
+            method: 'POST',
+            body: JSON.stringify({dataJson}),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if(!generateDoc.ok){
+            throw new Error('Error generating document');
+        }
+        const blob = await generateDoc.blob();
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = 'FLHA.pdf';
+        a.click();
+        a.remove();
+        window.URL.revokeObjectURL(url);
+    }catch(error){
+        console.error('Error generating document:', error);
+    }
+};
+export default handlePDFDownload;
