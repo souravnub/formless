9d976191671027f0fabae7cbe9df062e003ea3ef
souravnub <s6hr6h4u3@gmail.com>
2024-11-12 00:08:14 -0700

    refactor: pie chart removed dead code

--- src/components/charts/formAnalysis/CustomPieChart.tsx
+++ src/components/charts/formAnalysis/CustomPieChart.tsx
@@ -4,31 +4,8 @@ import { Label, Pie, PieChart } from "recharts";
 import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from "@/components/ui/chart";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
-[
-    { browser: "chrome", visitors: 275, fill: "var(--color-chrome)" },
-    { browser: "safari", visitors: 200, fill: "var(--color-safari)" },
-    { browser: "firefox", visitors: 287, fill: "var(--color-firefox)" },
-    { browser: "edge", visitors: 173, fill: "var(--color-edge)" },
-    { browser: "other", visitors: 190, fill: "var(--color-other)" },
-];
-
 const CustomPieChart = ({ question, data }: any) => {
     const totalResponses = data.reduce((p: number, c: any) => p + c.count, 0);
-
-    const chartConfig = {
-        yes: {
-            label: "Yes",
-            color: "hsl(var(--chart-1))",
-        },
-        no: {
-            label: "No",
-            color: "hsl(var(--chart-2))",
-        },
-        na: {
-            label: "NA",
-            color: "hsl(var(--chart-3))",
-        },
-    } satisfies ChartConfig;
 
     const config: any = {};
     data.forEach((obj: any, idx: number) => {
@@ -38,8 +15,11 @@ const CustomPieChart = ({ question, data }: any) => {
     data = data.map((ob: any, id: number) => {
         return { ...ob, fill: `var(--color-${ob.answer})` };
     });
-    console.log(data);
-    console.log(config);
+
+    if (totalResponses === 0) {
+        return <></>;
+    }
+
     return (
         <Card className="w-fit">
             <CardHeader className="items-center pb-0">
