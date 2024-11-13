112834aa3b63c374ae5c0ca4ba3b7680e01f3a84
souravnub <s6hr6h4u3@gmail.com>
2024-11-13 13:03:10 -0700

    chore: added dev script to /server

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,40 +6,40 @@ import os from "os";
 const httpServer = http.createServer();
 
 const io = new Server(httpServer, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
-        allowedHeaders: ["my-custom-header"],
-        credentials: true,
-    },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
 });
 io.on("connection", (socket) => {
-    console.log("A user connected: ", socket.id);
-    socket.on("join_room", (roomId) => {
-        socket.join(roomId);
-        console.log(`User ${socket.id} joined room: ${roomId}`);
-    });
+  console.log("A user connected: ", socket.id);
+  socket.on("join_room", (roomId) => {
+    socket.join(roomId);
+    console.log(`User ${socket.id} joined room: ${roomId}`);
+  });
 
-    socket.on("send_message", (data) => {
-        socket.to(data.roomId).emit("receive_message", data);
-    });
-    socket.on("disconnect", () => {
-        console.log("A user Disconnected:", socket.id);
-    });
+  socket.on("send_message", (data) => {
+    socket.to(data.roomId).emit("receive_message", data);
+  });
+  socket.on("disconnect", () => {
+    console.log("A user Disconnected:", socket.id);
+  });
 });
 const PORT = Number(process.env.PORT) || 3005;
 httpServer.listen(PORT, "0.0.0.0", undefined, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // Retrieve network interfaces and log IP addresses
 const interfaces = os.networkInterfaces();
 
 for (const interfaceName in interfaces) {
-    // TODO: What is type of interfaces[interfaceName]?
-    for (const net of interfaces[interfaceName] as any) {
-        if (net.family === "IPv4" && !net.internal) {
-            console.log(`Server IP Address: ${net.address}:${PORT}`);
-        }
+  // TODO: What is type of interfaces[interfaceName]?
+  for (const net of interfaces[interfaceName] as any) {
+    if (net.family === "IPv4" && !net.internal) {
+      console.log(`Server IP Address: ${net.address}:${PORT}`);
     }
+  }
 }
