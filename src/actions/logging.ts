830acbed610d240716e63999ff3797f5955ed247
rikuteq <151947637+rikuteq@users.noreply.github.com>
2024-11-15 16:05:44 -0700

    fixed an oversight in the undo functionality

--- src/actions/logging.ts
+++ src/actions/logging.ts
@@ -92,127 +92,133 @@ export const undoLog = async (logId: string) => {
     if (!log) {
         return { success: false, message: "Log not found" };
     }
-
-    switch (log.action) {
-        case "CREATE":
-            if (log.objectType === "USER") {
+    try {
+        switch (log.action) {
+            case "CREATE":
+                if (log.objectType === "USER") {
+                    await prisma.user.delete({
+                        where: {
+                            id: log.objectId,
+                        },
+                    });
+                }
+                if (log.objectType === "FORM") {
+                    await prisma.form.delete({
+                        where: {
+                            id: log.objectId,
+                        },
+                    });
+                }
+                if (log.objectType === "FORM_SUBMISSION") {
+                    await prisma.formSubmission.delete({
+                        where: {
+                            id: log.objectId,
+                        },
+                    });
+                }
+                if (log.objectType === "USER_REQUEST") {
+                    await prisma.userRequest.delete({
+                        where: {
+                            id: log.objectId,
+                        },
+                    });
+                }
+                break;
+    
+            case "UPDATE":
+                if (log.objectType === "USER") {
+                    await prisma.user.update({
+                        where: {
+                            id: log.objectId,
+                        },
+                        data: log.prevState as Prisma.UserUpdateInput,
+                    });
+                }
+                if (log.objectType === "FORM") {
+                    await prisma.form.update({
+                        where: {
+                            id: log.objectId,
+                        },
+                        data: log.prevState as Prisma.FormUpdateInput,
+                    });
+                }
+                if (log.objectType === "FORM_SUBMISSION") {
+                    await prisma.formSubmission.update({
+                        where: {
+                            id: log.objectId,
+                        },
+                        data: log.prevState as Prisma.FormSubmissionUpdateInput,
+                    });
+                }
+                if (log.objectType === "USER_REQUEST") {
+                    await prisma.userRequest.update({
+                        where: {
+                            id: log.objectId,
+                        },
+                        data: log.prevState as Prisma.UserRequestUpdateInput,
+                    });
+                }
+                break;
+    
+            case "DELETE":
+                if (log.objectType === "USER") {
+                    await prisma.user.create({
+                        data: log.prevState as Prisma.UserCreateInput,
+                    });
+                }
+                if (log.objectType === "FORM") {
+                    await prisma.form.create({
+                        data: log.prevState as Prisma.FormCreateInput,
+                    });
+                }
+                if (log.objectType === "FORM_SUBMISSION") {
+                    await prisma.formSubmission.create({
+                        data: log.prevState as Prisma.FormSubmissionCreateInput,
+                    });
+                }
+                if (log.objectType === "USER_REQUEST") {
+                    await prisma.userRequest.create({
+                        data: log.prevState as Prisma.UserRequestCreateInput,
+                    });
+                }
+                break;
+    
+            case "APPROVE":
+                await prisma.userRequest.create({
+                    data: log.prevState as Prisma.UserRequestCreateInput,
+                });
+    
                 await prisma.user.delete({
                     where: {
                         id: log.objectId,
                     },
                 });
-            }
-            if (log.objectType === "FORM") {
-                await prisma.form.delete({
-                    where: {
-                        id: log.objectId,
-                    },
+                break;
+    
+            case "DENY":
+                await prisma.userRequest.create({
+                    data: log.prevState as Prisma.UserRequestCreateInput,
                 });
-            }
-            if (log.objectType === "FORM_SUBMISSION") {
+                break;
+    
+            case "SUBMIT":
                 await prisma.formSubmission.delete({
                     where: {
                         id: log.objectId,
                     },
                 });
-            }
-            if (log.objectType === "USER_REQUEST") {
-                await prisma.userRequest.delete({
-                    where: {
-                        id: log.objectId,
-                    },
-                });
-            }
-            break;
-
-        case "UPDATE":
-            if (log.objectType === "USER") {
-                await prisma.user.update({
-                    where: {
-                        id: log.objectId,
-                    },
-                    data: log.prevState as Prisma.UserUpdateInput,
-                });
-            }
-            if (log.objectType === "FORM") {
-                await prisma.form.update({
-                    where: {
-                        id: log.objectId,
-                    },
-                    data: log.prevState as Prisma.FormUpdateInput,
-                });
-            }
-            if (log.objectType === "FORM_SUBMISSION") {
-                await prisma.formSubmission.update({
-                    where: {
-                        id: log.objectId,
-                    },
-                    data: log.prevState as Prisma.FormSubmissionUpdateInput,
-                });
-            }
-            if (log.objectType === "USER_REQUEST") {
-                await prisma.userRequest.update({
-                    where: {
-                        id: log.objectId,
-                    },
-                    data: log.prevState as Prisma.UserRequestUpdateInput,
-                });
-            }
-            break;
-
-        case "DELETE":
-            if (log.objectType === "USER") {
-                await prisma.user.create({
-                    data: log.prevState as Prisma.UserCreateInput,
-                });
-            }
-            if (log.objectType === "FORM") {
-                await prisma.form.create({
-                    data: log.prevState as Prisma.FormCreateInput,
-                });
-            }
-            if (log.objectType === "FORM_SUBMISSION") {
-                await prisma.formSubmission.create({
-                    data: log.prevState as Prisma.FormSubmissionCreateInput,
-                });
-            }
-            if (log.objectType === "USER_REQUEST") {
-                await prisma.userRequest.create({
-                    data: log.prevState as Prisma.UserRequestCreateInput,
-                });
-            }
-            break;
-
-        case "APPROVE":
-            await prisma.userRequest.create({
-                data: log.prevState as Prisma.UserRequestCreateInput,
-            });
-
-            await prisma.user.delete({
-                where: {
-                    id: log.objectId,
-                },
-            });
-            break;
-
-        case "DENY":
-            await prisma.userRequest.create({
-                data: log.prevState as Prisma.UserRequestCreateInput,
-            });
-            break;
-
-        case "SUBMIT":
-            await prisma.formSubmission.delete({
-                where: {
-                    id: log.objectId,
-                },
-            });
-            break;
-
-        default:
-            return { success: false, message: "Error has occured" };
+                break;
+    
+            default:
+                return { success: false, message: "Error has occured" };
+    }
+    }
+    catch (err) {
+        console.log(err);
+        return { success: false, message: "Log cannot be undone. Deleting log..." };
     }
     return { success: true, message: "Log undone successfully" };
+    
 };
 
 type GetLogsProps = {
