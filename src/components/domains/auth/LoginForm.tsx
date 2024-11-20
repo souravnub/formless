3fb7facd0116d9d3cdf79b4da595ce627d87d62f
AmrinderSinghKhangura <amrinder.khangura@edu.sait.ca>
2024-11-20 13:52:03 -0700

    login form and sign in form have option to see their inputted password

--- src/components/domains/auth/LoginForm.tsx
+++ src/components/domains/auth/LoginForm.tsx
@@ -7,67 +7,91 @@ import { useToast } from "@/hooks/use-toast";
 import { signIn } from "next-auth/react";
 import { Label } from "@/components/ui/label";
 import { useRouter } from "next/navigation";
+import { set } from "date-fns";
 
 const LoginForm = () => {
-    const { toast } = useToast();
-    const router = useRouter();
-    const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [hidden, setHidden] = useState(true);
+  const [passwordType, setPasswordType] = useState("password");
 
-    async function handleLoginFormAction(e: FormEvent) {
-        e.preventDefault();
-        const formData = new FormData(e.target as HTMLFormElement);
-        const email = formData.get("email") as string | undefined;
-        const password = formData.get("password") as string | undefined;
+  async function handleLoginFormAction(e: FormEvent) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const email = formData.get("email") as string | undefined;
+    const password = formData.get("password") as string | undefined;
 
-        if (!email || !password) {
-            return toast({
-                variant: "destructive",
-                title: "Invalid inputs",
-                description: "Please provide all the credentials",
-            });
-        }
-        setIsLoading(true);
-        const res = await signIn("credentials", {
-            email,
-            password,
-            redirect: false,
-        });
-        if (res?.error) {
-            setIsLoading(false);
-            return toast({
-                variant: "destructive",
-                title: "Invalid Credentials",
-                description: "The credentials provided were incorrect. Please Try with valid credentials",
-            });
-        }
-        router.push("/");
+    if (!email || !password) {
+      return toast({
+        variant: "destructive",
+        title: "Invalid inputs",
+        description: "Please provide all the credentials",
+      });
     }
+    setIsLoading(true);
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+    if (res?.error) {
+      setIsLoading(false);
+      return toast({
+        variant: "destructive",
+        title: "Invalid Credentials",
+        description:
+          "The credentials provided were incorrect. Please Try with valid credentials",
+      });
+    }
+    router.push("/");
+  }
 
-    return (
-        <form className="grid w-full items-center gap-4" onSubmit={handleLoginFormAction}>
-            <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="email">Email</Label>
-                <Input id="name" name="email" disabled={isLoading} />
-            </div>
-            <div>
-                <Label htmlFor="password">Password</Label>
-                <Input disabled={isLoading} id="password" type="password" name="password" />
-            </div>
+  return (
+    <form
+      className="grid w-full items-center gap-4"
+      onSubmit={handleLoginFormAction}
+    >
+      <div className="flex flex-col space-y-1.5">
+        <Label htmlFor="email">Email</Label>
+        <Input id="name" name="email" disabled={isLoading} />
+      </div>
+      <div className="relative">
+        <Label htmlFor="password">Password</Label>
 
-            <Button type="submit" disabled={isLoading} className="px-8 w-min">
-                {isLoading ? "loading..." : "Login"}
-            </Button>
+        <Input
+          className="w-full"
+          disabled={isLoading}
+          id="password"
+          type={passwordType}
+          name="password"
+        />
 
-            <div>
-                <p className="text-sm text-center">
-                    Don't have an account?{" "}
-                    <a href="/signup" className="text-primary hover:underline">
-                        Sign up
-                    </a>
-                </p>
-            </div>
-        </form>
-    );
+        <Label
+          onClick={() => {
+            setHidden(!hidden);
+            setPasswordType(hidden ? "text" : "password");
+          }}
+          className=" absolute right-2 bottom-1 hover:cursor-pointer hover:bg-black hover:text-white p-2  rounded-xl"
+        >
+          {hidden ? "show" : "hide"}
+        </Label>
+      </div>
+
+      <Button type="submit" disabled={isLoading} className="px-8 w-min">
+        {isLoading ? "loading..." : "Login"}
+      </Button>
+
+      <div>
+        <p className="text-sm text-center">
+          Don't have an account?{" "}
+          <a href="/signup" className="text-primary hover:underline">
+            Sign up
+          </a>
+        </p>
+      </div>
+    </form>
+  );
 };
 
 export default LoginForm;
--- src/components/domains/auth/SignupForm.tsx
+++ src/components/domains/auth/SignupForm.tsx
@@ -9,114 +9,142 @@ import { createRequest, getRequestByEmail } from "@/actions/userRequests";
 import { useRouter } from "next/navigation";
 import { getUserByEmail } from "@/actions/users";
 
-
 const SignupForm = () => {
-    const { toast } = useToast();
-    const [isLoading, setIsLoading] = useState(false);
-    const router = useRouter();
-    
-    async function handleSignup (e: React.FormEvent)  {
-        e.preventDefault();
-        const formData = new FormData(e.target as HTMLFormElement);
-        const name = formData.get("name") as string;
-        const email = formData.get("email") as string;
-        const password = formData.get("password") as string;
-        const confirmPassword = formData.get("confirmPass") as string;
-        const reqAlreadyExists = await getRequestByEmail(email);
-        const userAlreadyExists = await getUserByEmail(email);
-        
-        if (reqAlreadyExists && "email" in reqAlreadyExists && reqAlreadyExists.email === email) {
-            return toast({
-                variant: "destructive",
-                title: "Invalid inputs",
-                description: "User with this email already exists",
-            });
-        }
+  const { toast } = useToast();
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const [hidden, setHidden] = useState(true);
+  const [passwordType, setPasswordType] = useState("password");
 
-        if (userAlreadyExists && "email" in userAlreadyExists && userAlreadyExists.email === email) {
-            return toast({
-                variant: "destructive",
-                title: "Invalid inputs",
-                description: "User with this email already exists",
-            });
-        }
+  async function handleSignup(e: React.FormEvent) {
+    e.preventDefault();
+    const formData = new FormData(e.target as HTMLFormElement);
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const confirmPassword = formData.get("confirmPass") as string;
+    const reqAlreadyExists = await getRequestByEmail(email);
+    const userAlreadyExists = await getUserByEmail(email);
 
-        if (!name || !email || !password || !confirmPassword) {
-            return toast({
-                variant: "destructive",
-                title: "Invalid inputs",
-                description: "Please provide all the credentials",
-            });
-        }
+    if (
+      reqAlreadyExists &&
+      "email" in reqAlreadyExists &&
+      reqAlreadyExists.email === email
+    ) {
+      return toast({
+        variant: "destructive",
+        title: "Invalid inputs",
+        description: "User with this email already exists",
+      });
+    }
 
-        if(password !== confirmPassword) {
-            return toast({
-                variant: "destructive",
-                title: "Invalid inputs",
-                description: "Passwords do not match",
-            });
-        }
-        setIsLoading(true);
-        try {
-            const data = { name, email: email.toLowerCase(), password };
-            const res = await createRequest(data);
+    if (
+      userAlreadyExists &&
+      "email" in userAlreadyExists &&
+      userAlreadyExists.email === email
+    ) {
+      return toast({
+        variant: "destructive",
+        title: "Invalid inputs",
+        description: "User with this email already exists",
+      });
+    }
 
-            if (!res?.success) {
-                return toast({
-                    variant: "destructive",
-                    title: "Error",
-                    description: res?.message,
-                });
-            }
+    if (!name || !email || !password || !confirmPassword) {
+      return toast({
+        variant: "destructive",
+        title: "Invalid inputs",
+        description: "Please provide all the credentials",
+      });
+    }
 
-            toast({
-                variant: "default",
-                title: "Success",
-                description: "Successfully created request",
-            });
+    if (password !== confirmPassword) {
+      return toast({
+        variant: "destructive",
+        title: "Invalid inputs",
+        description: "Passwords do not match",
+      });
+    }
+    setIsLoading(true);
+    try {
+      const data = { name, email: email.toLowerCase(), password };
+      const res = await createRequest(data);
 
-            router.push("/login");
-        } catch (error) {
-        console.log(error);
-        }
-    };
-    
-    return (
-        <form
-            className="grid w-full items-center gap-4"
-            onSubmit={handleSignup}
-            >
-            <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" name="name" />
-            </div>    
-            <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="email">Email</Label>
-                <Input id="email" name="email" />
-            </div>
-            <div>
-                <Label htmlFor="password">Password</Label>
-                <Input id="password" type="password" name="password" />
-            </div>
-            <div>
-                <Label htmlFor="password">Confirm Password</Label>
-                <Input id="confirmPass" type="password" name="confirmPass" />
-            </div>
+      if (!res?.success) {
+        return toast({
+          variant: "destructive",
+          title: "Error",
+          description: res?.message,
+        });
+      }
 
-            <Button type="submit" disabled={isLoading} className="px-8 w-min">
-                {isLoading ? "loading..." : "Make Request"}
-            </Button>
+      toast({
+        variant: "default",
+        title: "Success",
+        description: "Successfully created request",
+      });
 
-            <div>
-                <p className="text-sm text-center">
-                    Already have an account?{" "}
-                    <a href="/login" className="text-primary hover:underline">
-                        Login
-                    </a>
-                </p>
-            </div>
-        </form>
-    );
-;}
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  return (
+    <form className="grid w-full items-center gap-4" onSubmit={handleSignup}>
+      <div className="flex flex-col space-y-1.5">
+        <Label htmlFor="name">Name</Label>
+        <Input id="name" name="name" />
+      </div>
+      <div className="flex flex-col space-y-1.5">
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" name="email" />
+      </div>
+      <div>
+        <Label htmlFor="password">Password</Label>
+        <div className="relative">
+          <Input id="password" type={passwordType} name="password" />
+          <Label
+            onClick={() => {
+              setHidden(!hidden);
+              setPasswordType(hidden ? "text" : "password");
+            }}
+            className=" absolute right-2 bottom-1 hover:cursor-pointer hover:bg-black hover:text-white p-2  rounded-xl"
+          >
+            {hidden ? "show" : "hide"}
+          </Label>
+        </div>
+      </div>
+      <div>
+        <Label htmlFor="password">Confirm Password</Label>
+        <div className="relative">
+          <Input id="confirmPass" type={passwordType} name="confirmPass" />
+          <Label
+            onClick={() => {
+              setHidden(!hidden);
+              setPasswordType(hidden ? "text" : "password");
+            }}
+            className=" absolute right-2 bottom-1 hover:cursor-pointer hover:bg-black hover:text-white p-2  rounded-xl"
+          >
+            {hidden ? "show" : "hide"}
+          </Label>
+        </div>
+      </div>
+
+      <Button type="submit" disabled={isLoading} className="px-8 w-min">
+        {isLoading ? "loading..." : "Make Request"}
+      </Button>
+
+      <div>
+        <p className="text-sm text-center">
+          Already have an account?{" "}
+          <a href="/login" className="text-primary hover:underline">
+            Login
+          </a>
+        </p>
+      </div>
+    </form>
+  );
+};
 
 export default SignupForm;
